5f61a9dce3a2e43233e0afdb82412b79c6ed308e
John C. Kushmer <12129071+piqey@users.noreply.github.com>
2020-02-11 15:42:18 -0500

    Changed WayMap to Waymap

--- lua/autorun/sh_waymap.lua
+++ lua/autorun/sh_waymap.lua
@@ -1,28 +1,28 @@
 AddCSLuaFile()
 
-WayMap = WayMap or {}
+Waymap = Waymap or {}
 
-WayMap.INSTANCE = {}
-WayMap.INSTANCE.SHARED = 1
-WayMap.INSTANCE.SERVER = 2
-WayMap.INSTANCE.CLIENT = 3
+Waymap.INSTANCE = {}
+Waymap.INSTANCE.SHARED = 1
+Waymap.INSTANCE.SERVER = 2
+Waymap.INSTANCE.CLIENT = 3
 
 --Easier way to include files
-function WayMap.Include(path, instance)
+function Waymap.Include(path, instance)
 	if SERVER then
-		if instance == WayMap.INSTANCE.SHARED or instance == WayMap.INSTANCE.CLIENT then
+		if instance == Waymap.INSTANCE.SHARED or instance == Waymap.INSTANCE.CLIENT then
 			AddCSLuaFile(path)
 		end
 
-		if instance == WayMap.INSTANCE.SHARED or instance == WayMap.INSTANCE.SERVER then
+		if instance == Waymap.INSTANCE.SHARED or instance == Waymap.INSTANCE.SERVER then
 			include(path)
 		end
 	end
 
-	if CLIENT and (instance == WayMap.INSTANCE.SHARED or instance == WayMap.INSTANCE.CLIENT) then
+	if CLIENT and (instance == Waymap.INSTANCE.SHARED or instance == Waymap.INSTANCE.CLIENT) then
 		include(path)
 	end
 end
 
-WayMap.Include("waymap/sh_config.lua", WayMap.INSTANCE.SHARED)
-WayMap.Include("waymap/sv_wmanager.lua", WayMap.INSTANCE.SERVER)
+Waymap.Include("Waymap/sh_config.lua", Waymap.INSTANCE.SHARED)
+Waymap.Include("Waymap/sv_wmanager.lua", Waymap.INSTANCE.SERVER)
