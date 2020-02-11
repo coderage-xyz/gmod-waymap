1086694969e24cd6f7add36d1e6a0494d7d4062e
John C. Kushmer <12129071+piqey@users.noreply.github.com>
2020-02-11 15:41:51 -0500

    Optimized function usage and remembered to spawn managers

--- lua/waymap/sv_wmanager.lua
+++ lua/waymap/sv_wmanager.lua
@@ -4,17 +4,34 @@
 
 local PLAYER = FindMetaTable("Player")
 
+function PLAYER:MakeWManager()
+	self.WManager = ents.Create("waypoint_manager")
+	self.WManager:SetPos(ply:GetPos())
+	self.WManager:SetParent(ply)
+	self.WManager:Spawn()
+	
+	return self.WManager
+end
+
 function PLAYER:GetWManager()
 	local wmanager = self.WManager
 	
 	if not IsValid(wmanager) then
-		self.WManager = ents.Create("waypoint_manager")
-		self.WManager:SetPos(ply:GetPos())
-		self.WManager:SetParent(ply)
-		self.WManager:Spawn()
-		
-		wmanager = self.WManager
+		wmanager = self:MakeWManager()
 	end
 	
 	return wmanager
 end
+
+--[[
+	Give players a waypoint manager on spawn
+--]]
+
+hook.Add("PlayerSpawn", "Waymap.SpawnWaypointManager", function(ply)
+	if not IsValid(ply:GetWManager()) then
+		ply:MakeWManager()
+		print("Made waypoint manager " .. tostring(ply:GetWManager()))
+	else
+		print("Player manager is valid, doing nothing.")
+	end
+end)
