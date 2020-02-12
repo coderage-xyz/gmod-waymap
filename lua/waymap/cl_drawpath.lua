0d4f1508b9daf65ed35884b4b6577f4dc692587f
John C. Kushmer <12129071+piqey@users.noreply.github.com>
2020-02-11 19:02:58 -0500

    Added commented sprite code

--- lua/waymap/cl_drawpath.lua
+++ lua/waymap/cl_drawpath.lua
@@ -3,13 +3,21 @@
 --]]
 
 local arrowmat = Material("waymap/arrow_outline")
+local wpmat = Material( "sprites/sent_ball" )
 
 hook.Add("PostDrawOpaqueRenderables", "Waymap.DrawPath", function()
 	local active = Waymap.Path.GetActive()
 	local last
 	
-	for _, node in pairs(active) do
+	for k, node in pairs(active) do
 		node = node + Vector(0, 0, 8)
+		
+		if (k == 1) or (k == #active) then
+			cam.Start3D2D(node, Angle(0, 0, 0), 1)
+				surface.SetMaterial(wpmat)
+				surface.DrawTexturedRect(-16, -16, 32, 32)
+			cam.End3D2D()
+		end
 		
 		if (last) then
 			render.SetMaterial(arrowmat)
