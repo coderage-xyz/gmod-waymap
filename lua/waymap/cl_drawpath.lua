9927cc80217f919f234e4d93ce44de8bd412559d
John C. Kushmer <12129071+piqey@users.noreply.github.com>
2020-02-11 19:04:49 -0500

    Fixed an error-spewing function

--- lua/waymap/cl_drawpath.lua
+++ lua/waymap/cl_drawpath.lua
@@ -7,17 +7,22 @@ local wpmat = Material( "sprites/sent_ball" )
 
 hook.Add("PostDrawOpaqueRenderables", "Waymap.DrawPath", function()
 	local active = Waymap.Path.GetActive()
+	
+	if not active then return end
+	
 	local last
 	
 	for k, node in pairs(active) do
 		node = node + Vector(0, 0, 8)
 		
+		--[[
 		if (k == 1) or (k == #active) then
 			cam.Start3D2D(node, Angle(0, 0, 0), 1)
 				surface.SetMaterial(wpmat)
 				surface.DrawTexturedRect(-16, -16, 32, 32)
 			cam.End3D2D()
 		end
+		--]]
 		
 		if (last) then
 			render.SetMaterial(arrowmat)
