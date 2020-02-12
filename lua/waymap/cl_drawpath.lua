887d62d365c63f112640bf3723bba5349dca568b
John C. Kushmer <12129071+piqey@users.noreply.github.com>
2020-02-12 11:04:47 -0500

    Added waypoint start and endpoint sprites

--- lua/waymap/cl_drawpath.lua
+++ lua/waymap/cl_drawpath.lua
@@ -3,7 +3,14 @@
 --]]
 
 local arrowmat = Material("waymap/arrow_outline")
---local wpmat = Material( "sprites/sent_ball" )
+local waypointmat = Material("sprites/sent_ball")
+
+local waypointstartcolor = Color(255, 154, 0)
+local waypointendcolor = Color(39, 167, 216)
+
+local waypointsize = 16
+
+local ptoffset = Vector(0, 0, 16)
 
 hook.Add("PostDrawOpaqueRenderables", "Waymap.DrawPath", function()
 	local active = Waymap.Path.GetActive()
@@ -31,14 +38,20 @@ hook.Add("PostDrawOpaqueRenderables", "Waymap.DrawPath", function()
 	end
 	--]]
 	
+	render.SetMaterial(waypointmat)
+	render.DrawSprite(active[1] + ptoffset, waypointsize, waypointsize, waypointstartcolor)
+	
 	--local last
 	render.SetMaterial(arrowmat)
 	render.StartBeam(#active)
 		for i, node in pairs(active) do
-			node = node + Vector(0, 0, 16)
+			node = node + ptoffset
 			local texcoord = (0.1 * Waymap.Path.GetTotalLength(active) / #active)
 			render.AddBeam(node, 8, (i * texcoord), color_white)
 			--last = node
 		end
 	render.EndBeam()
+	
+	render.SetMaterial(waypointmat)
+	render.DrawSprite(active[#active] + ptoffset, waypointsize, waypointsize, waypointendcolor)
 end)
