bdecb00582fbe8a5ee2b01a047ed9213851c76dc
John C. Kushmer <jckusho@gmail.com>
2020-02-15 16:58:50 -0500

    Updated sv_resource.lua

--- lua/waymap/sv_resource.lua
+++ lua/waymap/sv_resource.lua
@@ -2,5 +2,13 @@
 	Adding necessary downloads to FastDL
 --]]
 
+resource.AddFile("models/waymap/waypoint.mdl")
+resource.AddFile("models/waymap/waypoint_alt.mdl")
+resource.AddFile("models/waymap/arrow.mdl")
+resource.AddFile("models/waymap/arrow_indent.mdl")
+
+resource.AddFile("materials/models/arrow.vmt")
+resource.AddFile("materials/models/waypoint.vmt")
+
 resource.AddFile("materials/waymap/arrow.vmt")
 resource.AddFile("materials/waymap/arrow_outline.vmt")
