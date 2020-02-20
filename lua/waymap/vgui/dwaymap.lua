3674c96350bd47fc75dbff778ed20121b35b5e72
ThomasTerp <thomascterp@gmail.com>
2020-02-20 18:31:03 +0100

    Added dragging to map

--- lua/waymap/vgui/dwaymap.lua
+++ lua/waymap/vgui/dwaymap.lua
@@ -6,8 +6,10 @@ function PANEL:Init()
 	self.mapMaterial = nil
 	
 	self.mapViewPanel = vgui.Create("DPanel", self)
-	self.mapViewPanel.viewPositionX = 0
-	self.mapViewPanel.viewPositionY = -500
+	self.mapViewPanel.viewPositionX, self.mapViewPanel.viewPositionY = 0, 0
+	self.mapViewPanel.isMovingView = false
+	self.mapViewPanel.pressedCursorPositionX, self.mapViewPanel.pressedCursorPositionY = 0, 0
+	self.mapViewPanel.pressedViewPositionX, self.mapViewPanel.pressedViewPositionY = 0, 0
 	self.mapViewPanel.Paint = function(panel, width, height)
 		local camera = Waymap.Camera.GetLoaded()
 		
@@ -15,13 +17,34 @@ function PANEL:Init()
 			Waymap.Map.Draw(camera, self.mapMaterial, panel.viewPositionX, panel.viewPositionY, width)
 		end
 	end
+	self.mapViewPanel.OnMousePressed = function(panel, keyCode)
+		if keyCode == MOUSE_LEFT then
+			panel.pressedCursorPositionX, panel.pressedCursorPositionY = self:LocalCursorPos()
+			panel.pressedViewPositionX, panel.pressedViewPositionY = panel.viewPositionX, panel.viewPositionY
+			panel.isMovingView = true
+		end
+	end
+	self.mapViewPanel.Think = function(panel)
+		if panel.isMovingView then
+			if not input.IsMouseDown(MOUSE_LEFT) then
+				panel.isMovingView = false
+			end
+			
+			local cursorX, cursorY = self:LocalCursorPos()
+			
+			panel.viewPositionX = panel.pressedViewPositionX + (cursorX - panel.pressedCursorPositionX)
+			panel.viewPositionY = panel.pressedViewPositionY + (cursorY - panel.pressedCursorPositionY)
+		end
+	end
 end
 
 function PANEL:Think()
-	if not self.requestedMaterial and not self.mapMaterial and Waymap.Camera.loadedCamera then
+	local camera =  Waymap.Camera.GetLoaded()
+	
+	if not self.requestedMaterial and not self.mapMaterial and camera then
 		self.requestedMaterial = true
 		
-		Waymap.Map.Get(Waymap.Camera.loadedCamera, Waymap.Map.selectedMode, function(material)
+		Waymap.Map.Get(camera, Waymap.Map.selectedMode, function(material)
 			self.mapMaterial = material
 		end)
 	end
