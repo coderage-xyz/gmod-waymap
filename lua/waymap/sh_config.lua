f9ee27fd9d1c69dd77dc23c7822626e110114939
ThomasTerp <thomascterp@gmail.com>
2020-02-27 23:01:23 +0100

    Updated waypoint editor

--- lua/waymap/sh_config.lua
+++ lua/waymap/sh_config.lua
@@ -12,6 +12,20 @@ Waymap.Config = {
 	PlayerIndicatorSize = 64,
 	NodeSize = 32,
 	MapTextureSize = 2048,
+	IconModels = {
+		"models/Items/ammocrate_smg1.mdl";
+		"models/Items/BoxSRounds.mdl";
+		"models/Items/HealthKit.mdl";
+		"models/Items/item_item_crate.mdl";
+		"models/props_junk/TrafficCone001a.mdl";
+		"models/props_interiors/VendingMachineSoda01a.mdl";
+		"models/props_vehicles/carparts_wheel01a.mdl";
+		"models/props_c17/FurnitureCouch001a.mdl";
+		"models/props_junk/watermelon01.mdl";
+		"models/Gibs/HGIBS.mdl";
+		"models/props_c17/doll01.mdl";
+		"models/props_junk/garbage_takeoutcarton001a.mdl";
+	},
 	
 	----Permissions----
 	
