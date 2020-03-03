35bce83e7c070b865d2d2768cd97d9f0a8da959b
John C. Kushmer <jckusho@gmail.com>
2020-03-03 14:14:30 -0500

    Removed unused function

--- lua/waymap/debug/sh_debug.lua
+++ lua/waymap/debug/sh_debug.lua
@@ -4,10 +4,6 @@
 
 Waymap.Debug = Waymap.Debug or {}
 
-function Waymap.Debug.IsEnabled()
-	return Waymap.ConVars.Debug()
-end
-
 function Waymap.Debug.Print(...)
 	if not Waymap.ConVars.Debug() then return end
 	print(...)
